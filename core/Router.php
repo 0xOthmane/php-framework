0b1c267cccc5cde195596b6f4b6474b7beb50c73
0xOthmane <0xothodor@proton.me>
2023-11-29 14:30:26 +0100

    feat: add post method to Router

--- core/Router.php
+++ core/Router.php
@@ -6,12 +6,14 @@ class Router
 {
 
     public Request $request;
+    public Response $response;
     protected array $routes = [];
     // 2D Array ['get' => [$path=>$callback, ...], 'post' => [$path=>$callback, ...]]
 
-    public function __construct($request)
+    public function __construct($request, $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
 
@@ -20,13 +22,20 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
     public function resolve()
     {
-        $path= $this->request->getPath();
+        $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false; // Return a Closure Object
         if ($callback === false) {
-            echo 'Not found.'; exit;
+            // Application::$app->response->setStatusCode(404);
+            $this->response->setStatusCode(404);
+            return $this->renderView('_404');
         }
         if (is_string($callback)) {
             return $this->renderView($callback);
@@ -34,7 +43,16 @@ class Router
         echo call_user_func($callback); // echo the returned string by the callback
     }
 
-    public function renderView($view) {
+    // Render a string into the layout
+    public function renderViewContent($viewContent)
+    {
+        $layoutContent = $this->layoutContent();
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+        
+    }
+
+    public function renderView($view)
+    {
         $layoutContent = $this->layoutContent();
         $viewContent = $this->renderOnlyView($view);
         return str_replace('{{content}}', $viewContent, $layoutContent);
@@ -42,15 +60,17 @@ class Router
         // include_once Application::$ROOT_DIR."/views/$view.php";
     }
 
-    protected function layoutContent(){
+    protected function layoutContent()
+    {
         ob_start(); // caching output to browser
-        include_once Application::$ROOT_DIR."/views/layouts/mainLayout.php";
+        include_once Application::$ROOT_DIR . "/views/layouts/mainLayout.php";
         return ob_get_clean(); // Get current buffer contents and delete current output buffer
     }
 
-    protected function renderOnlyView($view) {
-        ob_start(); 
-        include_once Application::$ROOT_DIR."/views/$view.php";
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
         return ob_get_clean();
     }
 }
