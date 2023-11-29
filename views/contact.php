b38cec51faac65f9b699a5a5df937b7bc73fc631
0xOthmane <0xothodor@proton.me>
2023-11-29 14:31:44 +0100

    feat(ui): add form to contact view

--- views/contact.php
+++ views/contact.php
@@ -1,1 +1,16 @@
 <h1>Contact</h1>
+<form action="" method="POST">
+    <div class="mb-3">
+        <label class="form-label">Subject</label>
+        <input type="text" name="subject" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Email</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label class="form-label">Body</label>
+        <textarea type="text" name="body" class="form-control"> </textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
