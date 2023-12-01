fc6f1986ab949fe5f2dc0413fd96c7478578a467
0xOthmane <0xothodor@proton.me>
2023-12-01 13:47:43 +0100

    feat: add rules to validate model

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,20 +4,46 @@ namespace app\controllers;
 
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 
-class AuthController extends Controller {
+class AuthController extends Controller
+{
 
-    public function login() {
+    public function login()
+    {
         $this->setLayout('authLayout');
         return $this->render('login');
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
+        // $errors = [];
+
+        $registerModel = new RegisterModel();
         if ($request->isPost()) {
-            return 'Handle submitted data.';
+            $registerModel->loadData($request->getBody());
+            if ($registerModel->validate() && $registerModel->register()) {
+
+                return 'Success';
+            }
+            echo '<pre>';
+            var_dump($registerModel->errors);
+            echo '</pre>';
+            die;
+            return $this->render('register', [
+                'model' => $registerModel
+            ]);
+            // $firstname = $request->getBody()['firstname'];
+            // if (!$firstname) {
+            //     $errors['firstname'] = 'This field is requiered.';
+            // }
+            // return $this->render('register', [
+            //     'errors' => $errors
+            // ]);
         }
         $this->setLayout('authLayout');
-        return $this->render('register');
+        return $this->render('register', [
+            'model' => $registerModel
+        ]);
     }
-
 }
